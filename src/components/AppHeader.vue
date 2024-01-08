a89c7c7a191a0b0aa97fda91c5b08561623d3512
Gabrieleabu23 <kaidgabriele23@gmail.com>
2024-01-08 18:27:44 +0100

    add: yugioh logo and files scss, appheader in component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script >
+export default {
+    name:'AppHeader'
+}
+
+
+</script>
+
+<template>
+    
+</template>
+
+<style  scoped lang="scss">
+
+@import '../styles/general.scss';
+@import '../styles/partials/_variables';
+
+
+</style>
